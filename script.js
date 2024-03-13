fbcaa042d6a98e46fcc859e5375a844e6e855da7
davidev123 <163318653+davidev123@users.noreply.github.com>
2024-03-13 11:31:15 -0300

    Add files via upload
    Lançar o site.

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+}
+
+function closeSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+}  
